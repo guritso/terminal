467bc9e8b76bed3922adb6704bab45a8fc3cb44c
GuriZenit <gurizenit@gmail.com>
2024-09-24 21:08:23 -0300

    feat(types): declare types for typescript

---
+++ index.d.ts
@@ -0,0 +1,24 @@
+declare module "@guritso/terminal" {
+  export interface Terminal {
+    verbose: number;
+    readonly levels: {
+      readonly info: string;
+      readonly fail: string;
+      readonly pass: string;
+    };
+    projectInfo: {
+      name: string;
+      version: string;
+    };
+    start(host?: string, port?: number): void;
+    pass(message: unknown): void;
+    log(message: unknown): void;
+    setup(): boolean;
+    clear(): boolean;
+    setVerbose(verbose: number): void;
+    isError(data: unknown): boolean;
+  }
+
+  const terminal: Terminal;
+  export default terminal;
+}
